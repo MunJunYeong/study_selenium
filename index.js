9487f50bc262dfd33344b409773f9bab101a6645
MunJunYeong <>
2022-04-26 19:27:05 +0900

    update musinsa

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const {Builder, By, Key, until} = require('selenium-webdriver');
 
+const secret = require('./secret');
+
 const init = async () => {
     const driver = await new Builder()
     .forBrowser('chrome')
@@ -8,29 +10,52 @@ const init = async () => {
     return driver;
 }
 
-const delay = (time) => {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => { resolve();} ,time);
-    })
+const musinsa = async () => {
+    const driver = await init();
+
+    await driver.get('https://www.musinsa.com/app/');
+    await driver.executeScript("document.getElementsByClassName('header-member__login')[0].click()");
+
+    await driver.wait(until.elementLocated(By.className('login-input__input')), 10000);
+    await driver.executeScript(`document.getElementsByClassName('login-input__input')[0].value = '${secret.interpark.id}'`);
+    await driver.executeScript(`document.getElementsByClassName('login-input__input')[1].value = '${secret.interpark.pw}'`);
+
+    await driver.executeScript("document.getElementsByClassName('login-button__item login-button__item--black')[0].click()");
+
 }
 
-const start = async () => {
+const file = async () => {
+    const driver = await init();
+    await driver.get('https://www.filejo.com/');
+    await driver.SwitchTo().Frame(driver.FindElement(By.Id("Frame1")));
+    // await driver.wait(until.elementLocated(By.className('id_focus')), 10000);
+    await driver.executeScript(`document.getElementsByClassName('id_focus')[0].value = '${secret.interpark.id}'`);
+    // await driver.executeScript(`document.getElementsByName('mb_pw').value = '${secret.interpark.pw}'`);
+    
+}
+
+const interpark = async () => {
     const driver = await init();
     
     await driver.get('http://ticket.interpark.com/');
+    await driver.executeScript("document.getElementById('aLogin').click()");
+
+    await driver.switchTo().frame(0);
+    
 
     
-    // // const query = await driver.findElement({id: 'query'});
-    // await driver.executeScript("document.getElementById('userPwd').setAttribute('value','당산 맛집')");
-    await driver.executeScript("document.getElementById('aLogin').click()");
-    await driver.sleep(5000);
-    await delay(5000);
-    
-    
-    await driver.executeScript("document.getElementById('userId').value = 'hihi'");
-    await driver.executeScript("document.getElementById('userPwd').value = 'hihi'");
+    // await driver.wait(until.elementLocated(By.id('userId')), 10000);
+
+
+    //
+    await driver.executeScript(`document.getElementById('userId').value = '${secret.interpark.id}'`);
+    await driver.executeScript(`document.getElementById('userPwd').value = '${secret.interpark.pw}'`);
     await driver.executeScript("document.getElementById('btn_login').click()");
+
+    await driver.get('https://ticket.interpark.com/Contents/Sports/GoodsInfo?SportsCode=07032&TeamCode=PE015');
     
 };
 
-start();
+file();
+// musinsa();
+// interpark();
